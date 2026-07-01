3fbe5efed5256021878185dbbd2fefe913d31538
Arjun Manikandan <axm5507@gmail.com>
2026-07-01 14:34:36 -0500

    Update IReplacementPolicy.h with comments describing why it was added to project structure

--- include/IReplacementPolicy.h
+++ include/IReplacementPolicy.h
@@ -4,6 +4,10 @@
 #include "Config.h"
 #include <cstdint>
 #include <memory>
+//This class is a version 3 addition. It was not in the original project structure.
+//However, I found it necessary to add it to implement the replacement policies cleanly
+//and seperate them from the cache itself
+
 
 //During simulation, the cache calls 3 hooks: onLoad, onAccess, and selectVictim. 
 class IReplacementPolicy {
